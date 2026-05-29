f9ede542943653ffa5c5050e745dabf936fee54c
Robert Rozanski <robert_rozanski@icloud.com>
2026-05-29 16:44:13 +0200

    Bump version to 1.0.1 (6) for Play Store release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.robcish.smartfactswidget"
         minSdk = 23
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.0"
+        versionCode = 6
+        versionName = "1.0.1"
 
         buildConfigField(
             "String",
